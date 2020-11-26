0d3d6328c1df6f9345f384d55e06e418435843a3
gowtham <gowtham@creditmantri.com>
2020-11-26 20:41:49 +0530

    login page update

--- src/main/webapp/resources/js/login.js
+++ src/main/webapp/resources/js/login.js
@@ -5,8 +5,21 @@ else{
     console.log('not loaded yet');
 }
 
+addEventListener("load", function () {
+    setTimeout(hideURLbar, 0);
+}, false);
+
+function hideURLbar() {
+    window.scrollTo(0, 1);
+}
+
 $(document).ready(new function () {
     $(".login").click(function(){
       console.log("The Login Text was clicked.");
     });
+
+//    $(".facebook").click(function(){
+//      console.log("The Login Text was clicked.");
+//      window.location.href = "/user";
+//    });
 });
